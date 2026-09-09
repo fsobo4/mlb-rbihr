ed5175883cde00a946c2c0e3728a69876d323921
fsobo4 <fsobo15@gmail.com>
2026-09-09 15:30:54 -0400

    Fixed typo in api for backend hosting

--- api/pipeline.R
+++ api/pipeline.R
@@ -15,7 +15,7 @@ cors <- function(res) {
   plumber::forward()
 }
 
-db_path <- "/data/hr_data.db"
+db_path <- "/app/hr_data.db"
 query_db <- function(sql, params = list()) {
   con <- dbConnect(RSQLite::SQLite(), db_path)
   on.exit(dbDisconnect(con), add = TRUE)
